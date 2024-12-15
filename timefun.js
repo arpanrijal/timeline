c8e693ff8ba4ae7939a543e7969b10535cbbd1c9
Arpan Rijal <135034681+arpanrijal@users.noreply.github.com>
2024-12-15 12:24:54 +0545

    Update timefun.js
    Resolve AM PM not changing issue, value is out of scope issue, remove toggle, only get am pm update when 12 hrs format called, add new variable for more functionality

--- timefun.js
+++ timefun.js
@@ -2,30 +2,33 @@ const currentTime = new Date();
 let sec = currentTime.getSeconds();
 let minu = currentTime.getMinutes();
 let hrs = currentTime.getHours();
-let toggle = false;
-function onoff(value){
-    console.log(value)
-    if(value == 2)
-        {
-            if (hrs > 12) {
-              hrs = hrs % 12;
-            }
-          document.getElementById("hrs").innerHTML = hrs.toString().padStart(2, "0");
-          document.getElementById("am-pm").innerHTML = toggle ? "A.M." : "P.M.";
-
-      
-        }
-        else{
-            if(!toggle){
-                hrs = hrs + 12;
-            }
-            document.getElementById("hrs").innerHTML = hrs.toString().padStart(2, "0");
-          document.getElementById("am-pm").innerHTML = "";
-
-        }
+let store, hrs24, hrs12;
+function onoff(value) {
+  store = value;
+  if (value == 2) {
+    if (hrs > 12) {
+      hrs12 = hrs % 12;
+    }
+    document.getElementById("hrs").innerHTML = hrs.toString().padStart(2, "0");
   }
+  else {
+    if (hrs > 12) {
+      hrs24 = hrs + 12;
+    }
+    document.getElementById("hrs").innerHTML = hrs.toString().padStart(2, "0");
+    document.getElementById("am-pm").innerHTML = "";
+
+  }
+}
 setInterval(() => {
   sec++;
+  if(store == 2){
+    if (hrs>11) {
+      document.getElementById("am-pm").innerHTML = "P.M.";
+    } else {
+      document.getElementById("am-pm").innerHTML = "A.M.";
+    }
+  }
   if (sec >= 60) {
     sec = 0;
     minu++;
@@ -33,17 +36,15 @@ setInterval(() => {
       minu = 0;
       hrs++;
     }
-    if(hrs > 12 && value == 2){
-        hrs=1;
-        toggle = !toggle;
+    if (hrs > 12 && store == 2) {
+      hrs = 1;
     }
-    if(hrs > 24 && value == 1){
-        hrs=0;
-        toggle = !toggle;
+    if (hrs > 24 && store == 1) {
+      hrs = 0;
     }
   }
-  
-  
+
+
   document.getElementById("hrs").innerHTML = hrs.toString().padStart(2, "0");
   document.getElementById("sec").innerHTML = sec.toString().padStart(2, "0");
   document.getElementById("min").innerHTML = minu.toString().padStart(2, "0");
