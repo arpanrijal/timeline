a1787bcdb0376b7743825c33a766eaeccbb1f580
Arpan Rijal <135034681+arpanrijal@users.noreply.github.com>
2024-12-17 09:44:33 +0545

    Update timefun.js

--- timefun.js
+++ timefun.js
@@ -1,49 +1,26 @@
-const currentTime = new Date();
-let sec = currentTime.getSeconds();
-let minu = currentTime.getMinutes();
-let hrs = currentTime.getHours();
-let store, hrs24, hrs12;
-function onoff(value) {
-  store = value;
-  if (value == 2) {
-    if (hrs > 12) {
-      hrs12 = hrs % 12;
-    }
-  }
-  else {
-    if (hrs > 12) {
-      hrs24 = hrs + 12;
-    }
-    document.getElementById("hrs").innerHTML = hrs.toString().padStart(2, "0");
-    document.getElementById("am-pm").innerHTML = "";
-
-  }
-}
 setInterval(() => {
-  sec++;
-  if(store == 2){
-    if (hrs>11) {
+  const time = document.getElementById("timeGTM");
+  const currentTime = new Date();
+  let sec = currentTime.getSeconds();
+  let minu = currentTime.getMinutes();
+  let hrs24 = currentTime.getHours();
+  let hrs12 = currentTime.getHours();
+  if (Number(hrs24) > 12) {
+    hrs12 = currentTime.getHours() % 12;
+  }
+  if (time.value == 0) {
+    document.getElementById("hrs").innerHTML = hrs24
+      .toString()
+      .padStart(2, "0");
+    document.getElementById("am-pm").innerHTML = "";
+  } else {
+    document.getElementById("hrs").innerHTML = hrs12
+      .toString()
+      .padStart(2, "0");
+    if (Number(hrs12) > 11) {
       document.getElementById("am-pm").innerHTML = "P.M.";
     } else {
       document.getElementById("am-pm").innerHTML = "A.M.";
-    }
-    document.getElementById("hrs").innerHTML = hrs12.toString().padStart(2, "0");
-  }
-  else{
-    document.getElementById("hrs").innerHTML = hrs.toString().padStart(2, "0");
-  }
-  if (sec >= 60) {
-    sec = 0;
-    minu++;
-    if (minu >= 60) {
-      minu = 0;
-      hrs++;
-    }
-    if (hrs > 12 && store == 2) {
-      hrs = 1;
-    }
-    if (hrs > 24 && store == 1) {
-      hrs = 0;
     }
   }
   document.getElementById("sec").innerHTML = sec.toString().padStart(2, "0");
